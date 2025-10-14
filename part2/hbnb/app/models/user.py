1af22728939809a15a80362551620d556d6e7736
markjoehayes <markjoehayes@proton.me>
2025-10-14 14:56:52 -0400

    is_admin added to user class/test passed in shell

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -3,13 +3,14 @@ from app.models.base_model import BaseModel
 class User(BaseModel):
     """User class that iherits from BaseModel"""
 
-    def __init__(self, email="", password="", first_name="", last_name=""):
+    def __init__(self, email="", password="", first_name="", last_name="", is_admin=False):
         """Initialize User with provided default attributes"""
         super().__init__()
         self.email = email
         self.password = password
         self.first_name = first_name
         self.last_name = last_name
+        self.is_admin = is_admin
 
     def get_full_name(self):
         """Return the full name of the user"""
@@ -17,4 +18,5 @@ class User(BaseModel):
 
     def __str__(self):
         """Custom string representation"""
-        return f"{User} ({self.id}) {self.email} - {self.get_full_name()}"
+        admin_status = " (Admin)" if self.is_admin else ""
+        return f"User ({self.id}) {self.email} - {self.get_full_name()}{admin_status}"
