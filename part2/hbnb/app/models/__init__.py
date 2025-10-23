78651e9d1d05b5eb27d6b3f3507229a1eb2f6319
markjoehayes <markjoehayes@proton.me>
2025-10-23 09:33:52 -0400

    removed part3 dir

--- part2/hbnb/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# models/__init__.py
-from app.models.base_model import BaseModel
-from app.models.user import User
-from app.models.place import Place
-from app.models.amenity import Amenity
-#from app.models.review import Review
-# ... import other models
