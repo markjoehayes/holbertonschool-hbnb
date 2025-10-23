31ddb29653f4728a1f7a39cc261b000caaa9c1df
markjoehayes <markjoehayes@proton.me>
2025-10-23 09:26:22 -0400

    lose ends

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -9,10 +9,13 @@ def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
-    # Placeholder for API namespaces (endpoints will be added later)
     # Import and register namespaces
     from app.api.v1.amenities import api as amenities_ns
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    # Additional namespaces for places, reviews and amenities will be added later
+    from app.api.v1.reviews import api as reviews_ns
+    from app.api.v1.places import api as places_ns
 
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(places_ns, path='/api/v1/places')
+        
     return app
