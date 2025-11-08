89a98de27a81867809a0cba303597170e0c48b8c
markjoehayes <markjoehayes@proton.me>
2025-11-08 08:11:21 -0400

    register auth namespace!

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -47,11 +47,13 @@ def create_app(config_class=None):
 
     # Import and register namespaces
     from app.api.v1.users import api as users_ns
+    form app.api.v1.auth import as auth_ns
     from app.api.v1.amenities import api as amenities_ns
     from app.api.v1.reviews import api as reviews_ns
     from app.api.v1.places import api as places_ns
 
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(auth_ns, path='api/v1/auth')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(places_ns, path='/api/v1/places')
