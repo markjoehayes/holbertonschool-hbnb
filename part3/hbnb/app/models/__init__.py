92a407dad078f312c39384b6059edfbe37a7a4a9
markjoehayes <markjoehayes@proton.me>
2025-11-12 12:30:42 -0400

    change order of imports

--- part3/hbnb/app/models/__init__.py
+++ part3/hbnb/app/models/__init__.py
@@ -1,10 +1,11 @@
 # models/__init__.py
 from app.models.base_model import BaseModel
-from app.models.user import User
 from app.models.place import Place
+from app.models.review import Review
 from app.models.amenity import Amenity
+from app.models.user import User
 from app.models.storage import storage
-#from app.models.review import Review
+
 # ... import other models
 
 class Storage:
